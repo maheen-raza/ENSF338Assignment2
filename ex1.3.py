0e62d30b625a45295cdd7077c70a701c7d4ff6a8
maheen-raza <113572260+maheen-raza@users.noreply.github.com>
2023-02-10 15:28:08 -0700

    question 1 done

--- ex1.3.py
+++ ex1.3.py
@@ -1,1 +1,22 @@
+def func(n, memo={}):
+    if n == 0 or n == 1:
+        return n
+    if n in memo:
+        return memo[n]
+    else:
+        memo[n] = func(n-1, memo) + func(n-2, memo)
+        return memo[n]
 
+
+def interpolation_search(arr, x):
+    low = 0
+    high = len(arr) - 1
+    while low <= high and x >= arr[low] and x <= arr[high]:
+        pos = low + int(((float(high-low) / (arr[high] - arr[low])) * (x - arr[low])))
+        if arr[pos] == x:
+            return pos
+        if arr[pos] < x:
+            low = pos + 1
+        else:
+            high = pos - 1
+    return -1
--- ex1.5.py
+++ ex1.5.py
@@ -1,1 +1,34 @@
+import time
+import matplotlib.pyplot as plt
 
+def timing(func, n):
+    start = time.time()
+    result = func(n)
+    end = time.time()
+    return end - start
+
+def func1(n):
+    if n == 0 or n == 1:
+        return n
+    else:
+        return func1(n-1) + func1(n-2)
+
+def func2(n, memo={}):
+    if n == 0 or n == 1:
+        return n
+    if n in memo:
+        return memo[n]
+    else:
+        memo[n] = func2(n-1, memo) + func2(n-2, memo)
+        return memo[n]
+
+n_values = range(36)
+func1_times = [timing(func1, n) for n in n_values]
+func2_times = [timing(func2, n) for n in n_values]
+
+plt.plot(n_values, func1_times, label="Original Code")
+plt.plot(n_values, func2_times, label="Optimized Code")
+plt.legend()
+plt.xlabel("n (numbers 0-35)")
+plt.ylabel("Time (s)")
+plt.show()
