b9ca865076f62807fd72a129963a8e3fe81ba3f9
anitasjohn <anita.john@ucalgary.ca>
2023-02-12 21:04:58 -0600

    ex2.4 <3

--- ex2.4.py
+++ ex2.4.py
@@ -1,67 +1,67 @@
-<<<<<<< Updated upstream
-=======
-import json
 import sys
+import json
+import time
 import matplotlib.pyplot as plt
-import numpy as np
 
-from threading import stack_size
-stack_size(33554432)
-
-from time import perf_counter
 sys.setrecursionlimit(20000)
+
 def func1(arr, low, high):
-    if low < high:
+    store = []
+    storeappend= (low, high)
+    store.append(storeappend)
+    
+    while store:
+        low, high = store.pop()
+        if low<high:
+            pivot = func2(arr, low, high)
+            pivotlow = pivot-1
+            pivothigh = pivot +1
+            lower = (low, pivotlow)
+            higher = (high, pivothigh)
+            store.append(lower)
+            store.append(higher)
+    if low< high:
         pi = func2(arr, low, high)
         func1(arr, low, pi-1)
-        func1(arr, pi + 1, high)
+        func1(arr, pi+1, high)
+
 def func2(array, start, end):
-    p = array[start]
-    low = start + 1
-    high = end
+    middle = (end-start)//2
+    p = array[middle]
+    low = start+1
+    high = end 
     while True:
         while low <= high and array[high] >= p:
-            high = high - 1
-        while low <= high and array[low] <= p:
-            low = low + 1
-        if low <= high:
-            array[low], array[high] = array[high], array[low]
-        else:
+            high = high -1
 
+        while low <= high and array[low] <= p:
+            low = low+1
+
+        if low<= high:
+            array[low], array[high]=array[high], array[low]
+        else:
             break
     array[start], array[high] = array[high], array[start]
     return high
-# Load the input arrays from the JSON file
-with open("questiontwo.json") as f:
-    data = json.load(f)
-    #inputs = data["inputs"]
 
-# Sort each input array and print the result
-timingresult = []
-size = []
-for arr in data:
-    timestart = perf_counter()
-    low = 0
-    high = len(arr) - 1
-    
-    func1(arr, low, high)
-    timestop = perf_counter()
-    size.append(len(arr))
-    timingresult.append(timestop-timestart)
+timelist = []
+f = open("questiontwo.json")
+data = json.load(f)
 
 
+indice = list()
+time = list()
 
-print(timestop-timestart)
-plt.plot(size, timingresult)
-plt.xlabel("Input Size")
-plt.ylabel("Time (seconds)")
+for i in data:
+    start = time.time()
+    func1(i, 0, len(i)-1)
+    end = time.time()
+    indice.append(len(i))
+    time.append(end-start)
+
+plt.plot(indice, time)
+plt.xlabel("Index")
+plt.ylabel("Times")
+plt.title("QuickSort Times for Arrays")
 plt.show()
 
-##with open("sorted.json", "w") as outfile:
-   ##json.dump(data, outfile)
-
-
-"""
-Average-case time complexity: O(n log n)
-"""
->>>>>>> Stashed changes
